fe08a0d0dc5e5c8aca4b677a38a297485f1b4029
Colton Griffin <67336510+chemfinal-dot@users.noreply.github.com>
2022-01-21 17:51:38 -0500

    Add files via upload

---
+++ Approx_MATLAB_scripts/Approximating_Projections/star.m
@@ -0,0 +1,14 @@
+function [M,m,alpha] = star(n)
+% Returns star graph S_n
+% Independence number = 1 for n=1, n-1 else
+M = zeros(n);
+for i=2:n
+    M(1,i) = 1;
+    M(i,1) = 1;
+end
+[numRows, m] = size(M);
+alpha = n - 1;
+if (n == 1)
+    alpha = 1;
+end
+end
